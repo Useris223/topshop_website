72c961aa9a5830dc7814c5108716ba7dbd67d83c
Useris <useris420o@mgmail.com>
2026-01-20 04:59:31 +0200

    full site + enter + snow + music + game

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.get("/game")
+def game():
+    return render_template("game.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
